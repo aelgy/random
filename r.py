3392b78288f2a3825aaf04ab87ac33961d0f3ae9
Duke Chen <aelgy333@gmail.com>
2023-07-03 14:12:31 +0800

    Add count

--- r.py
+++ r.py
@@ -1,12 +1,14 @@
 import random
 
 r = random.randint(1, 100)
-
+count = 0
 while True:
+	count += 1
 	n = input('請輸入輸字（1~100）: ')
 	n = int(n)
 	if r == n:
 		print('終於猜對了')
+		print('總共猜了', count, '次')
 		break
 	elif r > n:
 		print('比答案小')
